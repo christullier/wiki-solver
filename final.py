14ea6fa71f0e2419a8c389851a29babaae537b48
averagephotographer <averagephotographer@users.noreply.github.com>
2021-06-16 01:42:37 -0500

    late night update

--- final.py
+++ final.py
@@ -1,5 +1,11 @@
 import json, requests
 
+
+
+# make links become children as soon as they're made?
+# make sure that self.views is not None
+
+
 class Article():
     def __init__(self, article_name):
         if article_name.startswith("https://") or article_name.startswith("en.wikipedia.org"):
@@ -11,11 +17,11 @@ class Article():
         self.links = self._get_links(self.name)
 
         # # of views
-        self.views = self._get_views(self.name)
+        self.views = self._get_views(self.links[:5])
 
     def _get_links(self, article_name):
         sub_links = []
-        page_object = self._get_request(article_name, "links")
+        page_object = self._get_request(article_name)
         # print(page_object)
         for page_id in page_object:
                 # main_title = page_object[page_id]['title']
@@ -27,23 +33,36 @@ class Article():
 
     
     def _get_views(self, article_list):
-        pass
+        page_object = self._get_request(article_list)
+        for page_id in page_object:
+            title = (page_object[page_id]['title'])
+            print(title)
+            sum = 0
+            for date in (page_object[page_id]['pageviews']):
+                daily_viewcount = page_object[page_id]['pageviews'][date]
+                sum += daily_viewcount
+            
+            print(sum)
+
 
     
     # article_list can be a list of one
     # returns page_object to be looped over
-    def _get_request(self, article_list, prop_type, limit = "max"):
-        if type(article_list) == list > 1:
+    def _get_request(self, article_list):
+        # from _get_list
+        if type(article_list) == list:
             titles = "|".join(article_list)
+            php = "https://en.wikipedia.org/w/api.php?action=query&prop=pageviews&format=json&titles=" + titles
+        # from _get_views
         elif (type(article_list) == str):
-            titles = article_list
+            title = article_list
+            php="https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&titles=" + title
         
         headers = {
             'User-Agent': 'wikipedia game solver',
             'From' : 'cdtv1473@gmail.com'
         }
-        php="https://en.wikipedia.org/w/api.php?action=query&prop=" + prop_type + "&pllimit="+ limit +"&format=json&titles=" + titles
-        print(php)
+        # print(php)
         response = requests.get(php, headers=headers)
         content = response.text
         json_object = json.loads(content)
@@ -60,4 +79,5 @@ if __name__ == "__main__":
     a = Article(name)
 
     print(a.name)
-    print(a.links)
+    # print(a.links)
+    print(a.views)
