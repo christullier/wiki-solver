408e149f127213dff03b6f9718c0700c86e8b12f
averagephotographer <averagephotographer@users.noreply.github.com>
2021-06-17 21:30:48 -0500

    fixed keyerror with json reqeust

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
 import json, requests, random, time
-random.seed(16)
-
+random.seed(26)
+DEBUG = False
 
 # make links become children as soon as they're made?
 # make sure that self.views is not None
@@ -16,10 +16,10 @@ class Article():
         # list of links
         self.links = self._get_links(self.name)
 
+        self.views = None
         # gets # of views for self and children article in same call
         self.random_links = random.sample(self.links, 48)
         self.random_links.append(self.name)
-        self.views = None
         self._get_views(self.random_links)
 
     #? may need an uplink function?
@@ -36,18 +36,16 @@ class Article():
     def _get_links(self, article_name):
         sub_links = []
         page_object = self._get_request(article_name)
-        # print(page_object)
         for page_id in page_object:
-                # main_title = page_object[page_id]['title']
                 for title in (page_object[page_id]['links']):
                     sub_article = title['title']
                     sub_links.append(sub_article)
-
         return sub_links
 
     
     def _get_views(self, article_list):
         page_object = self._get_request(article_list)
+        # print(page_object)
         for page_id in page_object:
             title = (page_object[page_id]['title'])
             if DEBUG == True: 
@@ -62,18 +60,18 @@ class Article():
                             sum += 0
                         else:
                             sum += daily_viewcount
-                    except:
+                    except KeyError:
                         if DEBUG == True:
-                            print()
                             print("view error")
                         sum += 0
-            except:
+                if self._wiki_format(title) == self.name:
+                    self.views = sum
+            except KeyError:
                 if DEBUG == True:
-                    print()
                     print("pageview error")
-            if self._wiki_format(title) == self.name:
-                self.views = sum
-            # print(sum)
+            if DEBUG == True:
+                print(sum)
+                                
 
 
     
@@ -85,30 +83,54 @@ class Article():
         if type(article_list) == list:
             titles = "|".join(article_list)
             # api call to get pageviews
-            php = "https://en.wikipedia.org/w/api.php?action=query&prop=pageviews&format=json&pvipcontinue&titles=" + titles
+            api = "https://en.wikipedia.org/w/api.php?action=query&prop=pageviews&format=json&pvipcontinue&titles=" + titles
 
         # if it's not a list, it's from _get_links, assume single string
         else:
             title = article_list
             # api to get page links
-            php="https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&titles=" + title
+            api="https://en.wikipedia.org/w/api.php?action=query&prop=links&pllimit=max&format=json&titles=" + title
         
         headers = {
             'User-Agent': 'wikipedia game solver',
             'From' : 'cdtv1473@gmail.com'
         }
-        response = requests.get(php, headers=headers)
+        response = requests.get(api, headers=headers)
         # for header in response.headers:
             # print(header + ' : ' + response.headers[header])
-        print()
+        print('.')
         content = response.text
+
         json_object = json.loads(content)
-        print(json_object['continue'])
+
+        # this checks to see if json_object has a 'continue' when getting the number of views 
+        # print(json_object['continue'])
+        # try:
+        
+                
+        if type(article_list) == list:
+            while 'continue' in json_object:
+                new_response = requests.get(api, headers=headers)
+                # for header in response.headers:
+                #     print(header + ' : ' + response.headers[header])
+        
+                # print(new_response.headers)
+                new_content = new_response.text
+                json_object = json.loads(new_content)
+                # print(json_object)
+                time.sleep(.5)
+                print('.')
+        
+        # while "pvipcontinue" in json_object['continue']:
+        #     self._get_request(article_list)
+            # print(json_object['continue'])
+        # except KeyError:
+            # pass
+
         query_object = json_object['query']
         page_object = query_object['pages']
     
         return page_object
-
 
 
 
