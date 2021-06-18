287449e3f18654b69eedd5cdf15a8bf45f3ced1d
averagephotographer <averagephotographer@users.noreply.github.com>
2021-06-17 23:24:03 -0500

    it works!!

--- final.py
+++ final.py
@@ -1,8 +1,9 @@
 import json, requests, random, time
-random.seed(26)
+# 28 gets stuck on emma watson and clint eastwood
+random.seed(29)
 DEBUG = False
 
-# make links become children as soon as they're made?
+# make links become children as 8oon as they're made?
 # make sure that self.views is not None
 
 
@@ -13,22 +14,24 @@ class Article():
         else:
             self.name = article_name
 
-        # list of links
-        self.links = self._get_links(self.name)
+        self.links = self._get_links(self.name)     # list of links
+        self.views = None   # number of views for this article
 
-        self.views = None
-        # gets # of views for self and children article in same call
-        self.random_links = random.sample(self.links, 48)
+        # gets number of views for self and children article in the same call
+        self.sample_size = 49
+        if self.sample_size > len(self.links):
+            self.sample_size = len(self.links)
+        self.random_links = random.sample(self.links, self.sample_size)
         self.random_links.append(self.name)
-        self._get_views(self.random_links)
+        
+        self.parent = None
+        self.best_child = None
 
     #? may need an uplink function?
     # links the parent to the child
     def __call__(self, child):
-        self.child = child
+        self.best_child = child
         child.parent = self
-        # links on page
-        # best link
 
     def _wiki_format(self, article_title):
         return article_title.replace(" ", "_")
@@ -41,39 +44,42 @@ class Article():
                     sub_article = title['title']
                     sub_links.append(sub_article)
         return sub_links
-
     
-    def _get_views(self, article_list):
+    def _get_best_sub_article(self, article_list):
         page_object = self._get_request(article_list)
+
+        max_views = 0
+        max_title = ""
         # print(page_object)
         for page_id in page_object:
             title = (page_object[page_id]['title'])
             if DEBUG == True: 
                 print(title)
             sum = 0
-            try:
-                for date in (page_object[page_id]['pageviews']):
-                    try:
-                        daily_viewcount = page_object[page_id]['pageviews'][date]
-                        # in case there are no views
-                        if daily_viewcount == None:
-                            sum += 0
-                        else:
-                            sum += daily_viewcount
-                    except KeyError:
-                        if DEBUG == True:
-                            print("view error")
-                        sum += 0
-                if self._wiki_format(title) == self.name:
-                    self.views = sum
-            except KeyError:
-                if DEBUG == True:
-                    print("pageview error")
+            for date in (page_object[page_id]['pageviews']):
+                
+                daily_viewcount = page_object[page_id]['pageviews'][date]
+                
+                # in case there are no views
+                if daily_viewcount == None:
+                    sum += 0
+                else:
+                    sum += daily_viewcount
+                sum += 0
+            
+            if self._wiki_format(title) == self.name:
+                self.views = sum
             if DEBUG == True:
                 print(sum)
-                                
+            if sum > max_views:
+                max_views = sum
+                max_title = title
 
-
+        # print(max_title)
+        # print(max_views)
+        # # self.best_child = (max_title)              
+        # print(">")
+        return Article(max_title)
     
     # article_list can be a list of one
     # returns page_object to be looped over
@@ -96,49 +102,67 @@ class Article():
             'From' : 'cdtv1473@gmail.com'
         }
         response = requests.get(api, headers=headers)
-        # for header in response.headers:
-            # print(header + ' : ' + response.headers[header])
-        print('.')
         content = response.text
 
         json_object = json.loads(content)
 
+       
         # this checks to see if json_object has a 'continue' when getting the number of views 
-        # print(json_object['continue'])
-        # try:
-        
-                
         if type(article_list) == list:
             while 'continue' in json_object:
                 new_response = requests.get(api, headers=headers)
-                # for header in response.headers:
-                #     print(header + ' : ' + response.headers[header])
         
-                # print(new_response.headers)
                 new_content = new_response.text
                 json_object = json.loads(new_content)
-                # print(json_object)
+
                 time.sleep(.5)
-                print('.')
-        
-        # while "pvipcontinue" in json_object['continue']:
-        #     self._get_request(article_list)
-            # print(json_object['continue'])
-        # except KeyError:
-            # pass
+                # print('.')
 
         query_object = json_object['query']
         page_object = query_object['pages']
     
         return page_object
 
+    def printer(self):
+        current = self
+        print(current.name)
 
+        while current.best_child is not None:
+            current = current.best_child
+            print(current.name)
+            # time.sleep(1)
+
+    @staticmethod
+    def solver(left, right):
+        # check if lists match up
+        for item in left.links:
+            if item in right.links:
+                new = Article(item)
+                left(new)
+                new(right)
+                return
+        
+        # get best children/parents
+        left.best_child = left._get_best_sub_article(left.random_links)
+        left(left.best_child)
+
+
+        right.parent = right._get_best_sub_article(right.random_links)
+        right.parent(right)
+
+        Article.solver(left.best_child, right.parent)
 
 if __name__ == "__main__":
-    name = "Avengers_(comics)"
-    name2 = 'Harry_Potter'
-    a = Article(name2)
+    name = "Ghidra"
+    name2 = 'photosynthesis'
+    a = Article(name)
+    b = Article(name2)
 
-    print(a.name)
-    # print(a.links)
-    print(a.views)
+    # print(len(a.links))
+    # print(len(b.links))
+
+    Article.solver(a, b)
+    # print(a.name)
+    # print(a.best_child.name)
+    # print(a.best_child.best_child.name)
+    a.printer()
